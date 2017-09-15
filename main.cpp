691ec2a9943688022d5e87bcc98e9716d8da33b2
Anthony Hake <lvlup97@gmail.com>
2017-09-15 15:28:34 -0400

    main.cpp
    updated so that introduction can be repeated upon invalid input

--- main.cpp
+++ main.cpp
@@ -1,29 +1,18 @@
 /*
 DEV TONY
 CREATED 8/11
-LAST MOD 8/11
+LAST MOD 8/15
 */
-#include <iostream>
-#include "functions.cpp"
-using namespace std;
+#include "header.h" 
 //Introduces user to the program with opening dialogue
 	void introduction()
 	{
+	int userChoice;
 	cout << "Welcome to BASE, what would you like to do?" << endl; 
 	cout << "1 - Convert from BASE X to BASE TEN" << endl;
 	cout << "2 - Convert from BASE TEN to BASE X" << endl;
 	cout << "3 - Convert from DECIMAL to BINARY" << endl;
-	}
-int main()
-{
-	int userChoice;
-
-	
-	
-
-	//Prompts User
-	cin >> userChoice;
-		switch (userChoice)
+	switch (userChoice)
 		{
 			case '1': 
 				cout << "You chose to convert from BASE X to BASE TEN" << endl;
@@ -38,6 +27,13 @@ int main()
 				cout << "Entered an invalid choice. Use the number corresponding with the choices" << endl;
 				introduction();
 		}
+	cin >> userChoice;
+	}
+int main()
+{
+	//Prompts User
+	void introduction();
+		
 
 	return 0;
 }
