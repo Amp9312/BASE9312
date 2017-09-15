58aeab4698fd31d430f620273bf9efd330f4d9e2
Anthony Hake <lvlup97@gmail.com>
2017-09-15 15:43:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,34 +5,38 @@ LAST MOD 8/15
 */
 #include "header.h" 
 //Introduces user to the program with opening dialogue
-	void introduction()
+	int introduction()
 	{
 	int userChoice;
 	cout << "Welcome to BASE, what would you like to do?" << endl; 
 	cout << "1 - Convert from BASE X to BASE TEN" << endl;
 	cout << "2 - Convert from BASE TEN to BASE X" << endl;
 	cout << "3 - Convert from DECIMAL to BINARY" << endl;
+	cin >> userChoice;
 	switch (userChoice)
 		{
-			case '1': 
+			case 1: 
 				cout << "You chose to convert from BASE X to BASE TEN" << endl;
 				//Call convertX1 function
-			case '2':
+				break;
+			case 2:
 				cout << "You chose to convert from BASE TEN to BASE X" << endl; 
 				//Call convertX2 function
-			case '3':
+				break;
+			case 3:
 				cout << "You chose to convert from DECIMAL to BINARY" << endl;
 				//Call DECBIN function
+				break;
 			default: 
 				cout << "Entered an invalid choice. Use the number corresponding with the choices" << endl;
-				introduction();
+				break;
 		}
-	cin >> userChoice;
 	}
 int main()
 {
+	cout << "BASE has successfully booted" << endl;
 	//Prompts User
-	void introduction();
+	introduction();
 		
 
 	return 0;
