d2e69499ac78169c8f0f2efde0b0494c771b91e0
Anthony Hake <lvlup97@gmail.com>
2018-09-09 10:27:14 -0400

    Update header.h

--- header.h
+++ header.h
@@ -4,11 +4,19 @@ void convertX1()
 {
 	int userBASE;
 	int userNUM;
+	string endResult;
 	cout << "Enter the number you'd like to convert" << endl;
 	cin >> userNUM;
 	cout << "Enter the base this number is in" << endl; 
 	cin >> userBASE; 
-	//do stuff
+	/*
+	while (userNUM >= 0){
+	endResult += userNUM % userBASE
+	userNUM = userNUM / userBASE 
+	}
+	reverse endResult if backwards due to appended values
+	cout << endResult << endl;
+	*/
 }
 
 void convertX2()
